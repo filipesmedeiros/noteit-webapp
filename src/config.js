2d93e2785fadbee4cfca476a6022df44509c7491
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-04-01 13:46:12 +0100

    Fixed config for new deployment

--- src/config.js
+++ src/config.js
@@ -9,13 +9,13 @@ const dev = {
     },
     apiGateway: {
         REGION: 'eu-west-2',
-        URL: 'https://zkr5up7rq9.execute-api.eu-west-2.amazonaws.com/dev'
+        URL: 'https://tht4et63ag.execute-api.eu-west-2.amazonaws.com/dev'
     },
     cognito: {
         REGION: 'eu-west-2',
-        USER_POOL_ID: 'eu-west-2_HpGyDWIX9',
-        APP_CLIENT_ID: '1ds24v7glk3jtn6oodinus16qv',
-        IDENTITY_POOL_ID: 'eu-west-2:66c7fe5b-eb66-4c09-92c5-45f155d487fe'
+        USER_POOL_ID: 'eu-west-2_Sr1X8e8BY',
+        APP_CLIENT_ID: '7tre90gkb874rll9lo0faicihs',
+        IDENTITY_POOL_ID: 'eu-west-2:fc92a467-a1ad-4bbf-851d-350f31b2da5f'
     },
     STRIPE_KEY: 'pk_test_VP2Btjebn50kxpP9ltnGe5MS003pB5inAp'
 };
@@ -37,7 +37,7 @@ const prod = {
         APP_CLIENT_ID: '',
         IDENTITY_POOL_ID: 'eu-west-2:'
     },
-    STRIPE_KEY: 'pk_prod_'
+    STRIPE_KEY: 'pk_live_01xk8LJlLQUtLpTpJWvQdSbz00XUUTnN5P'
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod'
